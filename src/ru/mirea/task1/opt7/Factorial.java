1c3d6ba179a2dbb6789cdf1a4bd9a0c883b0d1b2
BibAss <byba003@mail.ru>
2021-09-15 13:50:16 +0300

    implemented factorial function

---
+++ src/ru/mirea/task1/opt7/Factorial.java
@@ -0,0 +1,44 @@
+package ru.mirea.task1.opt7;
+
+import java.util.Scanner;
+
+
+public class Factorial {
+    public static long a = 1;
+    public static void main(String[] args) {
+        System.out.println("Enter your number: ");
+        Scanner sc = new Scanner(System.in);
+        int i;
+
+        if(sc.hasNextInt())
+        {
+            i = sc.nextInt();
+            if(i > 0)
+            {
+                System.out.println("Factorial of your number is: ");
+                Fact(i);
+            }
+            else
+            {
+                System.out.println("Eror in input data");
+            }
+        }
+        else
+        {
+            System.out.println("Eror in input data");
+        }
+    }
+    public static void Fact(int n)
+    {
+        if (n > 0)
+        {
+            a = a * n;
+            n -= 1;
+            Fact(n);
+        }
+        else
+        {
+            System.out.println(a);
+        }
+    }
+}
