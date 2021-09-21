31ce535c006a41a2b42509729983fea48e571de6
BibAss <byba003@mail.ru>
2021-09-21 21:18:59 +0300

    Add realization for 10 first elements of harmonic series

--- src/ru/mirea/task1/opt5/HarmonicSeries.java
+++ src/ru/mirea/task1/opt5/HarmonicSeries.java
@@ -1,4 +1,13 @@
 package ru.mirea.task1.opt5;
 
 public class HarmonicSeries {
+    public static void main(String[] args)
+    {
+        System.out.println("First 10 elements of harmonic series:");
+        for (int i = 1; i < 11; i++)
+        {
+            float c = 1/(float)i;
+            System.out.print(String.format("%.3f",c)+"; ");
+        }
+    }
 }
