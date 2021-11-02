72fa204e9e366ca8b3394592d041b7cfcaacb6f3
BibAss <byba003@mail.ru>
2021-11-03 01:55:15 +0300

    add opt8-12

--- src/ru/mirea/task10/opt11.java
+++ src/ru/mirea/task10/opt11.java
@@ -1,4 +1,24 @@
 package ru.mirea.task10;
 
+import java.util.Scanner;
+
 public class opt11 {
+    public static int Abuse(String s) { return Count(s, 0, 0); }
+    public static int Count(String s, int counter, int i) {
+        if (i < s.length() && !(s.charAt(i) == '0' && s.charAt(i+1) == '0')) {
+            if (s.charAt(i) == '1') {
+                counter++;
+                i++;
+            } else {
+                i++;
+            }
+            return Count(s, counter, i);
+        }
+        return counter;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(Abuse(s));
+    }
 }
--- src/ru/mirea/task10/opt12.java
+++ src/ru/mirea/task10/opt12.java
@@ -1,4 +1,27 @@
 package ru.mirea.task10;
 
-public class opt12 {
+import java.io.IOException;
+import java.util.Scanner;
+
+public class opt12  {
+    public static void main(String[] args) throws IOException {
+        odd();
+    }
+
+    public static void odd() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        if(s != 0)
+        {
+            if(s % 2 != 0)
+            {
+                System.out.print(s);
+            }
+            odd();
+        }
+        else
+        {
+            return;
+        }
+    }
 }
