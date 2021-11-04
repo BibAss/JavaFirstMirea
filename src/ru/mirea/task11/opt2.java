8b96f4c01a14248bd826e05fa67b3d70ba515de8
BibAss <byba003@mail.ru>
2021-11-04 22:33:37 +0300

    ......

--- src/ru/mirea/task11/opt2.java
+++ src/ru/mirea/task11/opt2.java
@@ -1,9 +1,183 @@
 package ru.mirea.task11;
 
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class opt2 {
     public static void main(String[] args) {
+        new opt2.App("Task11_2");
+    }
 
+    public static class App extends JFrame{
+        App(String s){
+            super(s);
+
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setVisible(true);
+            setLocationRelativeTo(null);
+
+            Container container = getContentPane();
+
+            JLabel north = new JLabel("Север" );
+            north.setHorizontalAlignment(JLabel.CENTER);
+            north.setBorder(BorderFactory.createLineBorder(Color.black));
+            north.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    JOptionPane.showMessageDialog(north, "Сервер");
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+
+            JLabel east = new JLabel("Восток" );
+            east.setHorizontalAlignment(JLabel.CENTER);
+            east.setBorder(BorderFactory.createLineBorder(Color.black));
+            east.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    JOptionPane.showMessageDialog(east, "поток");
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+
+            JLabel south = new JLabel("Юг" );
+            south.setHorizontalAlignment(JLabel.CENTER);
+            south.setBorder(BorderFactory.createLineBorder(Color.black));
+            south.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    JOptionPane.showMessageDialog(south, "гю");
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+
+            JLabel west = new JLabel("Запад" );
+            west.setHorizontalAlignment(JLabel.CENTER);
+            west.setBorder(BorderFactory.createLineBorder(Color.black));
+            west.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    JOptionPane.showMessageDialog(west, "Отпад");
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+
+            JLabel center = new JLabel("Центр" );
+            center.setHorizontalAlignment(JLabel.CENTER);
+            center.setBorder(BorderFactory.createLineBorder(Color.black));
+            center.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    JOptionPane.showMessageDialog(center, "Мидл");
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+
+            container.add(north, BorderLayout.NORTH);
+            container.add(east, BorderLayout.EAST);
+            container.add(south, BorderLayout.SOUTH);
+            container.add(west, BorderLayout.WEST);
+            container.add(center);
+            // Открываем окно
+            pack();
+            setSize(300,300);
+        }
     }
 }
--- src/ru/mirea/task11/opt3.java
+++ src/ru/mirea/task11/opt3.java
@@ -1,4 +1,104 @@
 package ru.mirea.task11;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class opt3 {
+    public static void main(String[] args) {
+        new opt3.Application("Task11_3");
+    }
+
+    static class Application extends JFrame {
+
+        Application(String s) {
+            super(s);
+
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setVisible(true);
+
+            setSize(300,150);
+            setLayout(new FlowLayout());
+            JRadioButton cianButton = new JRadioButton("Циан");
+            cianButton.setForeground(Color.cyan);
+            add(cianButton);
+
+            JRadioButton magentaButton = new JRadioButton("Маджента");
+            magentaButton.setForeground(Color.magenta);
+            add(magentaButton);
+
+            JRadioButton greenButton = new JRadioButton("Зеленый");
+            greenButton.setForeground(Color.green);
+            add(greenButton);
+
+            ButtonGroup g1 = new ButtonGroup();
+            g1.add(cianButton);
+            g1.add(magentaButton);
+            g1.add(greenButton);
+
+            JRadioButton font1 = new JRadioButton("Dialog input");
+            font1.setFont(new Font(Font.DIALOG_INPUT, Font.PLAIN, 12));
+            add(font1);
+
+            JRadioButton font2 = new JRadioButton("Monospaced");
+            font2.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+            add(font2);
+
+            JRadioButton font3 = new JRadioButton("Sans serif");
+            font3.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+            add(font3);
+
+            ButtonGroup g2 = new ButtonGroup();
+            g2.add(font1);
+            g2.add(font2);
+            g2.add(font3);
+
+            TextField text = new TextField(20);
+            text.setFont(new Font(Font.DIALOG, Font.PLAIN, 12));
+            add(text);
+
+            cianButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    text.setForeground(Color.cyan);
+                }
+            });
+
+            magentaButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    text.setForeground(Color.magenta);
+                }
+            });
+
+            greenButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    text.setForeground(Color.green);
+                }
+            });
+
+            font1.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    text.setFont(font1.getFont());
+                }
+            });
+
+            font2.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    text.setFont(font2.getFont());
+                }
+            });
+
+            font3.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    text.setFont(font3.getFont());
+                }
+            });
+        }
+    }
 }
